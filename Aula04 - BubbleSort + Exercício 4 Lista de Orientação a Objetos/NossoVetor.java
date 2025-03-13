0a7084f100e3375d1c31574cfa7e8e8b573cf835
Evandro Ferraz <ferraz.evandro@outlook.com>
2025-03-13 15:07:20 -0300

    Update NossoVetor.java

--- Aula04 - BubbleSort + Exercício 4 Lista de Orientação a Objetos/NossoVetor.java
+++ Aula04 - BubbleSort + Exercício 4 Lista de Orientação a Objetos/NossoVetor.java
@@ -113,28 +113,25 @@ public class NossoVetor{
     public void bubbleSort(){
         // Teste de Mesa
         // vetor = [8, 72, 64, 12, 36]
-        // n = 10 ^ 2 = 100
+        // Complexidade quadrática (n^2) no pior caso, sendo n o tamanho do vetor de entrada
         for(int i = 1; i < vetor.length; i++){
             for(int j = 0; j < vetor.length-i; j++){
                 // 1.1 iteração: j = 0, vetor[j] = 8
-                // 1.2 iteração: j = 1, vetor[j] = 72
+                // 1.2 iteração: j = 1, vetor[j] = 72, 72 e 64 trocam de lugar
                     // vetor = [8, 64, 72, 12, 36]
-                // 1.3 iteração: j = 2, vetor[j] = 72
+                // 1.3 iteração: j = 2, vetor[j] = 72, 72 e 12 trocam de lugar
                     // vetor = [8, 64, 12, 72, 36]
-                // 1.4 iteração: j = 3, vetor[j] = 72
+                // 1.4 iteração: j = 3, vetor[j] = 72, 72 e 36 trocam de lugar
                     // vetor = [8, 64, 12, 36, 72]
                 // 2.1 iteração: j = 0, vetor[j] = 8
-                // 2.2 iteração: j = 1, vetor[j] = 64
+                // 2.2 iteração: j = 1, vetor[j] = 64, 64 e 12 trocam de lugar
                     // vetor = [8, 12, 64, 36, 72]
-                // 2.3 iteração: j = 2, vetor[j] = 64
+                // 2.3 iteração: j = 2, vetor[j] = 64, 64 e 36 trocam de lugar
                     // vetor = [8, 12, 36, 64, 72]
                 // 3.1 iteração: j = 0, vetor[j] = 8
                 // 3.2 iteração: j = 1, vetor[j] = 12
                 // 4.1 iteração: j = 0, vetor[j] = 8
                     // Ordenação concluída: vetor = [8, 12, 36, 64, 72]
-                // 1 Laço: teria n iterações
-                // 2 Laço: n-i iterações, n-i simplificado para n
-                // Complexidade: n^2, uma complexidade quadrática.
                 if(vetor[j] > vetor[j+1]){
                     int temp = vetor[j];
                     vetor[j] = vetor[j+1];
